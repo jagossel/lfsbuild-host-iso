56f81127660930e5cfe10f09869b5d014ab5e2d2
Jonathon Gosselin <jgosselin@startmail.com>
2024-08-02 09:01:34 -0400

    Update permissions to be read-only

--- lfsbuild/profiledef.sh
+++ lfsbuild/profiledef.sh
@@ -18,7 +18,9 @@ airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
 bootstrap_tarball_compression=(zstd -c -T0 --long -19)
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/root/version-check.sh"]="0:0:774"
-  ["/root/prepare-sources.sh"]="0:0:774"
-  ["/root/prepare-environment.sh"]="0:0:774"
+  ["/root/patch-source.csv"]="0:0:440"
+  ["/root/patch-source.csv"]="0:0:440"
+  ["/root/version-check.sh"]="0:0:550"
+  ["/root/prepare-sources.sh"]="0:0:550"
+  ["/root/prepare-environment.sh"]="0:0:550"
 )
